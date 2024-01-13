906298be650a1997ea8e013f070ef54571db7d9a
Jaeha Lee <wogkdkrm112@gmail.com>
2024-01-13 20:51:02 +0900

    feat: User DAO 생성

---
+++ src/main/java/com/todo/restful/user/UserDaoService.java
@@ -0,0 +1,23 @@
+package com.todo.restful.user;
+
+import org.springframework.stereotype.Component;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+@Component
+public class UserDaoService {
+
+    private static List<User> users = new ArrayList<>();
+
+    static {
+        users.add(new User(1, "Adam", LocalDate.now().minusYears(30)));
+        users.add(new User(2, "Eve", LocalDate.now().minusYears(25)));
+        users.add(new User(1, "Jim", LocalDate.now().minusYears(50)));
+    }
+
+    public List<User> findAll() {
+        return users;
+    }
+}
