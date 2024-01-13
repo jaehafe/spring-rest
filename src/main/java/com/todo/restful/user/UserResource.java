b90e865924c795ae07cf8870bde4915cb398888b
Jaeha Lee <wogkdkrm112@gmail.com>
2024-01-13 23:24:25 +0900

    feat: ResponseEntity

--- src/main/java/com/todo/restful/user/UserResource.java
+++ src/main/java/com/todo/restful/user/UserResource.java
@@ -1,7 +1,10 @@
 package com.todo.restful.user;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.List;
 
 @RestController
@@ -13,13 +16,11 @@ public class UserResource {
         this.service = service;
     }
 
-    //GET /users
     @GetMapping("/users")
     public List<User> retrieveAllUsers() {
         return service.findAll();
     }
 
-    //GET /users/1
     @GetMapping("/users/{id}")
     public User retrieveUser(@PathVariable int id) {
         return service.findOne(id);
@@ -27,7 +28,14 @@ public class UserResource {
 
     //POST /users
     @PostMapping("/users")
-    public void createUser(@RequestBody User user) {
-        service.save(user);
+    public ResponseEntity<User> createUser(@RequestBody User user) {
+        User savedUser = service.save(user);
+        URI location = ServletUriComponentsBuilder
+                .fromCurrentRequest()
+                .path("/{id}")
+                .buildAndExpand(savedUser.getId())
+                .toUri();
+
+        return ResponseEntity.created(location).build();
     }
 }
