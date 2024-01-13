4feab569d60076eeee9cf436e0e9ee05721095a3
Jaeha Lee <wogkdkrm112@gmail.com>
2024-01-13 21:13:04 +0900

    feat: retrieveUser - findOne

--- src/main/java/com/todo/restful/user/UserDaoService.java
+++ src/main/java/com/todo/restful/user/UserDaoService.java
@@ -5,6 +5,8 @@ import org.springframework.stereotype.Component;
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
 
 @Component
 public class UserDaoService {
@@ -14,10 +16,15 @@ public class UserDaoService {
     static {
         users.add(new User(1, "Adam", LocalDate.now().minusYears(30)));
         users.add(new User(2, "Eve", LocalDate.now().minusYears(25)));
-        users.add(new User(1, "Jim", LocalDate.now().minusYears(50)));
+        users.add(new User(3, "Jim", LocalDate.now().minusYears(50)));
     }
 
     public List<User> findAll() {
         return users;
+    }
+
+    public User findOne(int id) {
+        Predicate<? super User> predicate = user -> user.getId().equals(id);
+        return users.stream().filter(predicate).findFirst().get();
     }
 }
---
+++ src/main/java/com/todo/restful/user/UserResource.java
@@ -0,0 +1,29 @@
+package com.todo.restful.user;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public class UserResource {
+
+    private UserDaoService service;
+
+    public UserResource(UserDaoService service) {
+        this.service = service;
+    }
+
+    //GET /users
+    @GetMapping("/users")
+    public List<User> retrieveAllUsers() {
+        return service.findAll();
+    }
+
+    //GET /users/1
+    @GetMapping("/users/{id}")
+    public User retrieveUser(@PathVariable int id) {
+        return service.findOne(id);
+    }
+}
