e6823ff03b34578f792cbb4b2ec871f03ae95d44
Jaeha Lee <wogkdkrm112@gmail.com>
2024-01-13 20:08:43 +0900

    feat: get /hello-world-bean

---
+++ src/main/java/com/todo/restful/helloworld/HelloWorldBean.java
@@ -0,0 +1,25 @@
+package com.todo.restful.helloworld;
+
+public class HelloWorldBean {
+
+    private String message;
+
+    public HelloWorldBean(String message) {
+        this.message = message;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    @Override
+    public String toString() {
+        return "HelloWorldBean{" +
+                "message='" + message + '\'' +
+                '}';
+    }
+}
---
+++ src/main/java/com/todo/restful/helloworld/HelloWorldController.java
@@ -0,0 +1,18 @@
+package com.todo.restful.helloworld;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController {
+
+    @GetMapping( "/hello-world")
+    public String helloWorld() {
+        return "Hello World";
+    }
+
+    @GetMapping( "/hello-world-bean")
+    public HelloWorldBean helloWorldBean() {
+        return new HelloWorldBean("Hello World");
+    }
+}
