fd76adb9c705af98627d7d4b40e52119a885b9e0
Jaeha Lee <wogkdkrm112@gmail.com>
2024-01-14 15:26:39 +0900

    feat: i18n

--- src/main/java/com/todo/restful/helloworld/HelloWorldController.java
+++ src/main/java/com/todo/restful/helloworld/HelloWorldController.java
@@ -1,18 +1,30 @@
 package com.todo.restful.helloworld;
 
+import org.springframework.cglib.core.Local;
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContext;
+import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Locale;
+
 @RestController
 public class HelloWorldController {
 
-    @GetMapping( "/hello-world")
+    private MessageSource messageSource;
+
+    public HelloWorldController(MessageSource messageSource) {
+        this.messageSource = messageSource;
+    }
+
+    @GetMapping("/hello-world")
     public String helloWorld() {
         return "Hello World";
     }
 
-    @GetMapping( "/hello-world-bean")
+    @GetMapping("/hello-world-bean")
     public HelloWorldBean helloWorldBean() {
         return new HelloWorldBean("Hello World");
     }
@@ -20,8 +32,16 @@ public class HelloWorldController {
     //Path Parameters
     // /users/{id}/todos/{id} => /users/2/todos/200
 
-    @GetMapping( "/hello-world/path-variable/{name}")
+    @GetMapping("/hello-world/path-variable/{name}")
     public HelloWorldBean helloWorldPathVariable(@PathVariable String name) {
         return new HelloWorldBean(String.format("Hello World, %s", name));
+    }
+
+    @GetMapping(path = "/hello-world-internationalized")
+    public String helloWorldInternationalized() {
+        Locale locale = LocaleContextHolder.getLocale();
+        return messageSource.getMessage("good.morning.message", null, "Default Message", locale);
+
+//        return "Hello World V2";
     }
 }
