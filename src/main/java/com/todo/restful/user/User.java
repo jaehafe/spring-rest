e7149e04f276e4c5f01600e9cab42f7fe1c41d6f
Jaeha Lee <wogkdkrm112@gmail.com>
2024-01-20 22:03:01 +0900

    feat: connect h2 db

--- src/main/java/com/todo/restful/user/User.java
+++ src/main/java/com/todo/restful/user/User.java
@@ -1,19 +1,32 @@
 package com.todo.restful.user;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
 import jakarta.validation.constraints.Past;
 import jakarta.validation.constraints.Size;
 
 import java.time.LocalDate;
 
 
+@Entity(name = "user_details")
 public class User {
 
+    public User() {
+    }
+
+    @Id
+    @GeneratedValue
     private Integer id;
 
+
     @Size(min = 2, message = "Name should have at least 2 characters")
+    @JsonProperty("user-name")
     private String name;
 
     @Past(message = "Birth Date should be in the past")
+    @JsonProperty("birth_date")
     private LocalDate birthDate;
 
     public User(Integer id, String name, LocalDate birthDate) {
